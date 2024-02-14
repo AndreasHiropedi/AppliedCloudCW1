145e66a767896c2ba5e2553e93e6d4dd42ac09c4
AndreasHiropedi <s2015345@ed.ac.uk>
2024-02-14 16:03:25 +0000

    almost finished controller

--- src/main/java/com/coursework1/appliedcloudcw1/controller/CoreRestController.java
+++ src/main/java/com/coursework1/appliedcloudcw1/controller/CoreRestController.java
@@ -8,25 +8,30 @@ public class CoreRestController {
 
     private String value = "";
 
-    // Example GET endpoint returning a simple string
     @GetMapping("/uuid")
     public ResponseEntity<String> getStudentId() {
         // Return student ID embedded in <h1></h1> tag
-        String htmlResponse = "<h1>s2015345</h1>";
+        String htmlResponse = "<h1>s2015345</h1>" + "\n";
         return ResponseEntity.ok().body(htmlResponse);
     }
 
     @PostMapping("/writevalue")
     public ResponseEntity<?> writeValue(@RequestParam("value") String newValue) {
         // Overwrite the previous value and respond with HTTP 200 OK
-        value = newValue;
+        value = newValue + "\n";
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/readvalue")
     public ResponseEntity<String> readValue() {
         // Directly return the value. If it's not set, it will return an empty string.
-        return ResponseEntity.ok().contentType(org.springframework.http.MediaType.TEXT_PLAIN).body(value);
+        return ResponseEntity.ok().contentType(org.springframework.http.MediaType.TEXT_PLAIN).body(value+ "\n");
+    }
+
+    @PostMapping("/callservice")
+    public ResponseEntity<String> callService() {
+
+        return ResponseEntity.ok().body("");
     }
 
 }
